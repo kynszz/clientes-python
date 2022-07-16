e7e7014766c10b20b12f8624235b2f2dc3e473e6
Kynszz <106875528+kynszz@users.noreply.github.com>
2022-07-16 13:52:59 -0300

    att

---
+++ formulario.py
@@ -0,0 +1,7 @@
+import validacao
+# View - o que vai para usuario 
+def formulario_login():
+    usuario_digitado = input("infrome o seu usuario: ")
+    senha_digitado = input("informe sua senha: ")
+    validacao.validar_login(usuario_digitado, senha_digitado)
+
--- main.py
+++ main.py
@@ -1,30 +1,3 @@
-# View - o que vai para usuario 
-def view ():
-    usuario_digitado = input("infrome o seu usuario: ")
-    senha_digitado = input("informe sua senha: ")
-    controller(usuario_digitado, senha_digitado)
+import formulario
 
-# Model - o que vem do banco de dados (BD)
-def model_usuario():
-        usuario_BD = "joao"
-        return usuario_BD
-
-def model_senha():
-    senha_BD = '123'
-    return senha_BD
-
-# Controller - a validaçao
-def controller(usuario_digitado, senha_digitado):
-    usuario_BD = model_usuario()
-    senha_BD = model_senha()
-    if usuario_digitado == usuario_BD and senha_digitado == senha_BD:
-        print("pode entrar")
-    else:
-        print("usuario ou senha invalido")
-
-
-
-
-
-
-view()
+formulario.formulario_login()
