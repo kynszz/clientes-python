97849bba07c334884e345362e8677c5da92eb9f8
Kynszz <106875528+kynszz@users.noreply.github.com>
2022-07-16 13:09:42 -0300

    att

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 # View - o que vai para usuario 
-usuario_digitado = input("infrome o seu usuario: ")
-senha_digitado = input("informe sua senha: ")
+def view ():
+    usuario_digitado = input("infrome o seu usuario: ")
+    senha_digitado = input("informe sua senha: ")
+    controller(usuario_digitado, senha_digitado)
 
 # Model - o que vem do banco de dados (BD)
-usuario_BD = "joao"
-senha_BD = "123"
+def model_usuario():
+        usuario_BD = "joao"
+        return usuario_BD
+
+def model_senha():
+    senha_BD = '123'
+    return senha_BD
 
 # Controller - a validaçao
-if usuario_digitado == "usuario_BD" and senha_digitado == senha_BD:
-    print('pode entrar')
-else:
-    print("usuario ou senha invalido")
+def controller(usuario_digitado, senha_digitado):
+    usuario_BD = model_usuario()
+    senha_BD = model_senha()
+    if usuario_digitado == usuario_BD and senha_digitado == senha_BD:
+        print("pode entrar")
+    else:
+        print("usuario ou senha invalido")
+
+
+
+
+
+
+view()
